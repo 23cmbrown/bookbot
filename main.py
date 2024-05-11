abdfb759aab04048466e73d5ff219ca876116a2c
23cmbrown <23cmbrown@gmail.com>
2024-05-11 18:34:05 -0400

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     with open("//home//connerbrown//workspace//github.com//23cmbrown//bookbot//books//frankenstein") as f:
         file_contents = f.read()
+        
         print(file_contents)
 
 
