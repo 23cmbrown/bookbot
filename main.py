cd5d4d0819104dd1e2c8ec9e50105113a81063e3
23cmbrown <23cmbrown@gmail.com>
2024-05-11 15:00:41 -0400

    updating main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+def main():
+    with open("//home//connerbrown//workspace//github.com//23cmbrown//bookbot//books//frankenstein") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+
+main()
